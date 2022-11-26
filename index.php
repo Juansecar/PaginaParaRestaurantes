63aeaaf789b9dde2fe0a47f71d3b5a9e8a84536c
sebas <juansebastiancardona01@gmail.com>
2022-11-25 19:03:30 -0500

    mi primer proyecto

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Restaurante | Pensilvania-Caldas</title>
+	<link rel="stylesheet" href="./style.css">
+
+
+<h1 style="text-align:center;">Restaurante |Pensilvania-Caldas</h1>
+
+
+	<a href="staff" >Empleado</a>
+	<a href="admin">Administrador</a>
+
+
+	<img src="image/pensilviania.jpg" alt="imagen pensilvania"
+body background="pensilvania.jpg"/>
+
+
+
+</head>
+<body>
+</div>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+
+
+	
+</body>
+</html>
